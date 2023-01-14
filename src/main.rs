a5e776708a6117acc4a38d03d917194bb7a68170
Kryo098 <ryo0812eng@gmail.com>
2023-01-14 11:40:02 +0900

    メインの処理追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("数を当ててごらん");
+    println!("ほら、予想を入力してね");
+
+    let mut guess = string::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("行の読み込みに失敗しました");
+    println!("次のように予想しました：　{}", guess);
 }
